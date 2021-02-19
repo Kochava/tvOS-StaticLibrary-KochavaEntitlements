76d563ff9d5b5e15bcc05662025613d54ca542aa
John Bushnell <jbushnell@kochava.com>
2021-02-19 08:32:47 -0800

    4.2.0

---
+++ libKochavaEntitlementsTVOS/KochavaEntitlements.h
@@ -0,0 +1,27 @@
+//
+//  KochavaEntitlements.h
+//  KochavaEntitlements
+//
+//  Created by John Bushnell on 12/11/19.
+//  Copyright © 2019 - 2020 Kochava, Inc.  All rights reserved.
+//
+
+
+
+#ifndef KochavaEntitlements_h
+#define KochavaEntitlements_h
+
+
+
+#import "KochavaCore.h"
+
+#import "KVAEntitlements.h"
+#import "KVAEntitlementsProduct.h"
+#import "KVAReceipt.h"
+
+
+
+#endif
+
+
+
